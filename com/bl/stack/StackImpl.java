5396324c35c55566a5993f4c1ebb901bb5ff5aa2
Yashaswini192 <voruganti.yashaswini@ust.com>
2023-02-20 17:50:29 +0530

    Create Queue using linkedlist,enqueue the items to queue

--- com/bl/stack/StackImpl.java
+++ com/bl/stack/StackImpl.java
@@ -5,9 +5,9 @@ import java.util.LinkedList;
 public class StackImpl<S> {
 
 	public Node top;
-
 	public Node peek;
-
+	public Node temp;
+	
 	public void push(S item) {
 		Node<S>node = new Node(item);
 		if(top == null) {
@@ -28,9 +28,30 @@ public class StackImpl<S> {
 			top = top.next;
 		}
 	}
+
+	public void enqueue(S item) { //for adding items into queue
+		Node<S>node = new Node(item);
+		if(top == null) {
+			top = node;
+			temp = node;
+		}
+		else {
+			temp.next = node;
+			temp = node;
+		}
+	}
+		
+	public boolean isEmpty() {
+		if(top == null) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	 
 	@Override
 	public String toString() {
-		return "StackImpl [top=" + top + "]";
+		return "StackImpl [top=" + top + ", peek=" + peek + ", temp=" + temp + "]";
 	}
 
 }
--- com/bl/stack/StackMain.java
+++ com/bl/stack/StackMain.java
@@ -5,6 +5,8 @@ public class StackMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		StackImpl<Integer> S1 = new StackImpl();
+		//Stack Operations:
+		System.out.println("Stack Operations:");
 		S1.push(70);
 		System.out.println(S1.toString());
 
@@ -16,7 +18,7 @@ public class StackMain {
 
 		S1.pop(56);
 		System.out.println("\n"+S1.toString());
-
+ 
 		S1.pop(30);
 		System.out.println("\n"+S1.toString());
 
@@ -24,5 +26,22 @@ public class StackMain {
 		System.out.println("\n"+S1.toString());
 		S1.pop(45);
 
+		
+		//Queue Operations:
+		System.out.println("Queue Operations:");
+		S1.enqueue(56);
+		System.out.println("\n"+S1.toString());
+		
+		S1.enqueue(30);
+		System.out.println("\n"+S1.toString());
+		
+		S1.enqueue(70);
+		System.out.println("\n"+S1.toString());
+		
+		S1.isEmpty();
+		System.out.println("\n "+S1.isEmpty());
+		
+		
+		
 	}
 }
